ddced3e7030a6bdc14d93174333e0ae8ec00cab5
Adnaan Badr <adnaan.badr@taxfix.de>
2020-11-29 17:53:27 +0100

    remove form binding

--- api.go
+++ api.go
@@ -11,9 +11,7 @@ import (
 
 	"golang.org/x/crypto/bcrypt"
 
-	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
-	"github.com/mholt/binding"
 )
 
 const (
@@ -53,39 +51,6 @@ type API struct {
 	sessionStore SessionsStore
 }
 
-type SignupData struct {
-	Email    string                 `json:"email"`
-	Password string                 `json:"password"`
-	Metadata map[string]interface{} `json:"metadata"`
-}
-
-func (s *SignupData) Bind(_ *http.Request) error {
-	return nil
-}
-
-type LoginData struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func (l *LoginData) Bind(_ *http.Request) error {
-	return nil
-}
-
-// Fieldmap for the LoginData
-func (l *LoginData) FieldMap(_ *http.Request) binding.FieldMap {
-	return binding.FieldMap{
-		&l.Email: binding.Field{
-			Form:     "email",
-			Required: true,
-		},
-		&l.Password: binding.Field{
-			Form:     "password",
-			Required: true,
-		},
-	}
-}
-
 // hashPassword generates a hashed password from a plaintext string
 func hashPassword(password string) (string, error) {
 	pw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -95,185 +60,82 @@ func hashPassword(password string) (string, error) {
 	return string(pw), nil
 }
 
-func (a *API) Signup(w http.ResponseWriter, r *http.Request) {
-	signupData := &SignupData{Metadata: make(map[string]interface{})}
+func (a *API) Signup(email, password string, metadata map[string]interface{}) error {
 
-	contentType := r.Header.Get("Content-type")
-	if contentType == "" {
-		contentType = jsonContentType
+	if len(password) < 8 {
+		return fmt.Errorf("password length < 8")
 	}
 
-	switch contentType {
-	case formContentType:
-		r.ParseForm()
-		for k, v := range r.Form {
-
-			if k == "email" && len(v) == 0 {
-				render.Render(w, r, ErrInvalidRequest(fmt.Errorf("email is required")))
-				return
-			}
-
-			if k == "password" && len(v) == 0 {
-				render.Render(w, r, ErrInvalidRequest(fmt.Errorf("password is required")))
-				return
-			}
-
-			if len(v) == 0 {
-				continue
-			}
-
-			if k == "email" && len(v) > 0 {
-				signupData.Email = v[0]
-				continue
-			}
-
-			if k == "password" && len(v) > 0 {
-				signupData.Password = v[0]
-				continue
-			}
-
-			if len(v) == 1 {
-				signupData.Metadata[k] = v[0]
-				continue
-			}
-			if len(v) > 1 {
-				signupData.Metadata[k] = v
-			}
-		}
-
-	case jsonContentType:
-		if err := render.Bind(r, signupData); err != nil {
-			render.Render(w, r, ErrInvalidRequest(err))
-			return
-		}
-	}
-	if len(signupData.Password) < 8 {
-		render.Render(w, r, ErrInvalidRequest(fmt.Errorf("password length < 8")))
-		return
+	if !isEmailValid(email) {
+		return fmt.Errorf("invalid email")
 	}
 
-	if !isEmailValid(signupData.Email) {
-		render.Render(w, r, ErrInvalidRequest(fmt.Errorf("invalid email")))
-		return
-	}
-
-	hashedPassword, err := hashPassword(signupData.Password)
+	hashedPassword, err := hashPassword(password)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
-	id, err := a.userStore.New(signupData.Email, hashedPassword, signupData.Metadata)
+	id, err := a.userStore.New(email, hashedPassword, metadata)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	confirmationToken := uuid.New().String()
 	err = a.userStore.SaveConfirmationToken(id, confirmationToken)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	err = a.sendMail(fmt.Sprintf("http://localhost:4000/confirm/%s", confirmationToken))
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	err = a.userStore.SaveConfirmationTokenSentAt(id, time.Now())
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
-	switch contentType {
-	case formContentType:
-		http.Redirect(w, r, "/login?confirmation_sent=true", http.StatusSeeOther)
-		return
-	case jsonContentType:
-		render.Status(r, http.StatusOK)
-		return
-	}
+	return nil
 }
 
-func (a *API) Login(w http.ResponseWriter, r *http.Request) {
-	loginData := new(LoginData)
+func (a *API) Login(w http.ResponseWriter, r *http.Request, email, password string) error {
 
-	contentType := r.Header.Get("Content-type")
-	if contentType == "" {
-		contentType = jsonContentType
+	if len(password) < 8 {
+		return fmt.Errorf("password length < 8")
 	}
 
-	switch contentType {
-	case formContentType:
-		if errs := binding.Bind(r, loginData); errs != nil {
-			render.Render(w, r, ErrInvalidRequest(errs))
-			return
-		}
-
-	case jsonContentType:
-		if err := render.Bind(r, loginData); err != nil {
-			render.Render(w, r, ErrInvalidRequest(err))
-			return
-		}
-	}
-	if len(loginData.Password) < 8 {
-		render.Render(w, r, ErrInvalidRequest(fmt.Errorf("password length < 8")))
-		return
+	if !isEmailValid(email) {
+		return fmt.Errorf("invalid email")
 	}
 
-	if !isEmailValid(loginData.Email) {
-		render.Render(w, r, ErrInvalidRequest(fmt.Errorf("invalid email")))
-		return
-	}
-
-	id, err := a.userStore.UserIDByEmail(loginData.Email)
+	id, err := a.userStore.UserIDByEmail(email)
 	if err != nil {
-		render.Render(w, r, ErrInvalidRequest(err))
-		return
+		return err
 	}
-
-	fmt.Println("id ", id)
 
 	confirmed, err := a.userStore.IsEmailConfirmed(id)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	if !confirmed {
-		switch contentType {
-		case formContentType:
-			http.Redirect(w, r, "/login?not_confirmed=true", http.StatusSeeOther)
-			return
-		case jsonContentType:
-			render.JSON(w, r, map[string]interface{}{
-				"confirmed": false,
-			})
-			render.Status(r, http.StatusOK)
-			return
-		}
-		return
+		return fmt.Errorf("email not confirmed")
 	}
 
-	password, err := a.userStore.GetPassword(id)
+	existingPassword, err := a.userStore.GetPassword(id)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(loginData.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(existingPassword), []byte(password))
 	if err != nil {
-		render.Render(w, r, ErrUnauthorized(err))
-		return
+		return err
 	}
 
 	session, err := a.sessionStore.Get(r, "auth-session")
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	token := uuid.New().String()
@@ -281,31 +143,10 @@ func (a *API) Login(w http.ResponseWriter, r *http.Request) {
 	session.Values["token"] = token
 	err = session.Save(r, w)
 	if err != nil {
-		render.Render(w, r, ErrInternal(fmt.Errorf("session.Save %w", err)))
-		return
+		return err
 	}
 
-	email, metadata, err := a.userStore.UserData(id)
-	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
-	}
-
-	switch contentType {
-	case formContentType:
-		http.Redirect(w, r, "/app", http.StatusSeeOther)
-		return
-	case jsonContentType:
-		render.JSON(w, r, map[string]interface{}{
-			"id":       id,
-			"email":    email,
-			"metadata": metadata,
-			"token":    token,
-		})
-		render.Status(r, http.StatusOK)
-		return
-	}
-
+	return nil
 }
 
 func (a *API) Logout(w http.ResponseWriter, r *http.Request) {
@@ -379,27 +220,22 @@ func (a *API) LoggedInUser(r *http.Request) (string, string, map[string]interfac
 	return userID, email, metadata, nil
 }
 
-func (a *API) ConfirmEmail(w http.ResponseWriter, r *http.Request) {
-	token := chi.URLParam(r, "token")
+func (a *API) ConfirmEmail(token string) error {
 	id, err := a.userStore.UserIDByConfirmationToken(token)
 	if err != nil {
-		render.Render(w, r, ErrInvalidRequest(err))
-		return
+		return err
 	}
 
 	err = a.userStore.MarkConfirmed(id, true)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 	err = a.userStore.DeleteConfirmToken(id)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
-	http.Redirect(w, r, "/login?confirmed=true", http.StatusSeeOther)
-
+	return nil
 }
 
 func (a *API) ChangeEmail(id, newEmail string) error {
@@ -426,33 +262,28 @@ func (a *API) ChangeEmail(id, newEmail string) error {
 	return nil
 }
 
-func (a *API) ConfirmEmailChange(w http.ResponseWriter, r *http.Request) {
-	token := chi.URLParam(r, "token")
+func (a *API) ConfirmEmailChange(token string) error {
 	id, err := a.userStore.UserIDByEmailChangeToken(token)
 	if err != nil {
-		render.Render(w, r, ErrInvalidRequest(fmt.Errorf("invalid or expired token: %w", err)))
-		return
+		return fmt.Errorf("invalid or expired token: %w", err)
 	}
 
 	newEmail, err := a.userStore.GetEmailChange(id)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	err = a.userStore.UpdateEmail(id, newEmail)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
 	err = a.userStore.DeleteEmailChangeToken(id)
 	if err != nil {
-		render.Render(w, r, ErrInternal(err))
-		return
+		return err
 	}
 
-	http.Redirect(w, r, "/account?email_changed=true", http.StatusSeeOther)
+	return nil
 }
 
 func (a *API) Recovery(email string) error {
